7be84f4ca0c2432700b3de117c97b15281da826a
joshua <tt87882541@hotmail.com>
2025-11-18 21:54:45 +0900

    chore: change ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,14 @@ module.exports = {
       script: 'dist/main.js',
       env: {
         NODE_ENV: 'development',
+        ENV_FROM: 'system',
         PORT: '3000',
         AWS_REGION: 'ap-southeast-1',
         AWS_SECRET_NAME: '/dev/mintall-nft-cert-api',
       },
       env_production: {
         NODE_ENV: 'production',
+        ENV_FROM: 'aws',
         PORT: '3000',
         AWS_REGION: 'ap-southeast-1',
         AWS_SECRET_NAME: '/prod/mintall-nft-cert-api',
